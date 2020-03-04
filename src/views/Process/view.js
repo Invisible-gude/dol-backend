52d0b998acfa8043eae6ac0fce5500aaa7f41011
kladpiban112 <57986918+kladpiban112@users.noreply.github.com>
2020-03-04 16:59:50 +0700

    serach

--- src/views/Process/view.js
+++ src/views/Process/view.js
@@ -3,8 +3,9 @@ import { Card, CardHeader, Col, Row, CardBody,Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import swal from 'sweetalert';
 import ProcessModel from '../../models/ProcessModel';
-import { Table } from 'antd';
-
+import { Table , Input} from 'antd';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 
 var process_model = new ProcessModel();
 
@@ -13,7 +14,9 @@ class ProcessView extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      process_list: []
+      process_list: [],
+      searchText: '',
+      searchedColumn: '',
     };
   }
 
@@ -55,25 +58,76 @@ class ProcessView extends Component {
         }
       });
   }
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text =>
+    this.state.searchedColumn === dataIndex ? (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ) : (
+      text
+    ),
+});
+handleSearch = (selectedKeys, confirm, dataIndex) => {
+  confirm();
+  this.setState({
+    searchText: selectedKeys[0],
+    searchedColumn: dataIndex,
+  });
+};
+
+handleReset = clearFilters => {
+  clearFilters();
+  this.setState({ searchText: '' });
+};
 
   render() {
     const columns = [
-      // {
-      //     title: '#',
-      //     dataIndex: 'key',
-      //     key: 'key',
-      //     width: '10%',
-      //     render: (text, record, index,current) => (
-      //         <span key={index}>
-      //             {index + 1 }
-      //         </span>
-      //     )
-      // },
       {
           title: 'รหัสขั้นตอน',
           dataIndex: 'process_id',
           key: 'process_id',
           width: '25%',
+          ...this.getColumnSearchProps('process_id'),
           render: (text, record, index) =>(
             <span key={index}>
            {text}
@@ -85,6 +139,8 @@ class ProcessView extends Component {
         dataIndex: 'process_name',
         key: 'process_name',
         width: '25%',
+        ...this.getColumnSearchProps('process_name'),
+
         render: (text, record, index) =>(
           <span key={index}>
          {text}
@@ -96,6 +152,8 @@ class ProcessView extends Component {
         dataIndex: 'department_name',
         key: 'department_name',
         width: '25%',
+        ...this.getColumnSearchProps('department_name'),
+
         render: (text, record, index) =>(
           <span key={index}>
           {text}
--- src/views/Service/view.js
+++ src/views/Service/view.js
@@ -3,7 +3,9 @@ import { Card, CardHeader, Col, Row, CardBody,Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import swal from 'sweetalert';
 import ServiceModel from '../../models/ServiceModel';
-import { Table } from 'antd';
+import { Table,Input } from 'antd';
+import Highlighter from 'react-highlight-words';
+import { SearchOutlined } from '@ant-design/icons';
 
 var service_model = new ServiceModel();
 
@@ -12,7 +14,9 @@ class ServiceView extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      service_list: []
+      service_list: [],
+      searchText: '',
+      searchedColumn: '',
     };
   }
 
@@ -58,6 +62,67 @@ class ServiceView extends Component {
         }
       });
   }
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text =>
+    this.state.searchedColumn === dataIndex ? (
+      <Highlighter
+        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+        searchWords={[this.state.searchText]}
+        autoEscape
+        textToHighlight={text.toString()}
+      />
+    ) : (
+      text
+    ),
+});
+handleSearch = (selectedKeys, confirm, dataIndex) => {
+  confirm();
+  this.setState({
+    searchText: selectedKeys[0],
+    searchedColumn: dataIndex,
+  });
+};
+
+handleReset = clearFilters => {
+  clearFilters();
+  this.setState({ searchText: '' });
+};
 
   render() {
     const columns = [
@@ -67,6 +132,7 @@ class ServiceView extends Component {
         dataIndex:  'service_id',
         key: 'service_id',
         width: '25%',
+        ...this.getColumnSearchProps('service_id'),
         render: (text, record, index) =>(
           <span key={index}>
          {text}
@@ -78,6 +144,7 @@ class ServiceView extends Component {
           dataIndex:  'service_name',
           key: 'service_name',
           width: '25%',
+          ...this.getColumnSearchProps('service_name'),
           render: (text, record, index) =>(
             <span key={index}>
            {text}
@@ -89,6 +156,7 @@ class ServiceView extends Component {
         dataIndex: 'service_type_name',
         key: 'service_type_name',
         width: '25%',
+        ...this.getColumnSearchProps('service_type_name'),
         render: (text, record, index) =>(
           <span key={index}>
          {text}
