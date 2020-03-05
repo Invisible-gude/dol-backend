e9a86c87a59107c5538458b8171f0b048682bbc9
kladpiban112 <57986918+kladpiban112@users.noreply.github.com>
2020-03-05 09:36:35 +0700

    app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const MainLayout = React.lazy(() => import('./containers/MainLayout'));
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
 function App() {
   const user_login = JSON.parse(localStorage.getItem('user_login'));
+  console.log("login ", user_login )
   if (user_login == null) {
     return (
       <HashRouter>
@@ -28,7 +29,8 @@ function App() {
         <React.Suspense fallback={loading()}>
           <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet" />
           <Switch>
-            <Route path="/" name="Home" render={props => <MainLayout {...props} />} />
+            {user_login.department_name === 'admin'  ? <Route path="/" name="Home" render={props => <MainLayout {...props} />} /> : <Route exact path="/login" name="Login Page" render={props => <Login {...props} />} />}
+          
           </Switch>
         </React.Suspense>
       </HashRouter>
