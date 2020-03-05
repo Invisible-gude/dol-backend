f96cdcec779accb81603ee152d2227f323072933
Invisible-gudegude <59083810+invesible-gude@users.noreply.github.com>
2020-03-05 09:35:53 +0700

    add task folder

---
+++ src/views/Task/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+// Pages
+const TaskView = React.lazy(() => import('./view'));
+const TaskInsert = React.lazy(() => import('./insert'));
+const TaskUpdate = React.lazy(() => import('./update'));
+
+class DepartmentMain extends Component {
+
+  render() {
+    return (
+        <HashRouter>
+        <React.Suspense fallback={loading()}>
+          <Switch>
+          <Route exact path="/task" name="Login Page" render={props => <TaskView {...props} />} />
+            <Route exact path="/task/insert" name="Login Page" render={props => <TaskInsert {...props} />} />
+            <Route exact path="/task/update/:task_id" name="Login Page" render={props => <TaskUpdate {...props} />} />
+          </Switch>
+        </React.Suspense>
+      </HashRouter>
+    );
+  }
+}
+
+
+export default (DepartmentMain);
